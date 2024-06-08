d1684a6683329d5f7d54289acf096254df19dd2b
Eduardo Maravilla <eduardomaravillag@gmail.com>
2024-06-07 23:43:48 -0600

    Update Guess Random Number

--- guessrandomnumber/src/test/java/org/eduardomaravill/guessrandomnumber/GuessrandomnumberApplicationTests.java
+++ guessrandomnumber/src/test/java/org/eduardomaravill/guessrandomnumber/GuessrandomnumberApplicationTests.java
@@ -1,13 +1,44 @@
 package org.eduardomaravill.guessrandomnumber;
 
+import org.eduardomaravill.guessrandomnumber.controllers.RandomNumberController;
+import org.eduardomaravill.guessrandomnumber.dto.RandomNumberDto;
+import org.eduardomaravill.guessrandomnumber.services.IGenerateRandomNumber;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class GuessrandomnumberApplicationTests {
 
+	@InjectMocks
+	private RandomNumberController randomNumberController;
+
+    @Mock
+	private IGenerateRandomNumber generateRandomNumber;
+
+    @BeforeEach
+	public void init() {
+		MockitoAnnotations.openMocks(this);
+	}
+
 	@Test
+	@DisplayName("Random number")
 	void contextLoads() {
+		when(generateRandomNumber.getRandomNumber()).thenReturn(8);
+		ResponseEntity<RandomNumberDto> response = randomNumberController.randomNumber();
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+		assertEquals(8, Objects.requireNonNull(response.getBody()).getRandomNumber());
 	}
 
 }
